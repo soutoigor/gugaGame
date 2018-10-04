44fa5ce2343b1946b13d39a43c7f7eac6ed4ac08
Igor Souto <igor_souto@outlook.com>
2018-10-04 20:45:21 -0300

    update documentation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gugaGame
+
+## O que é
+Este é um simples jogo criado para uma aula da matéria de Programação para Jogos na faculdade.
+A ideia era resolver o exercício criando um jogo arcade de raquete, onde devia ter pontos de acertos e erros,  sons e usando o oCanvas.
+Acabei gostando, me simpatizando com o joguinho e dei uma dedicada um pouco maior :grin:
+O nome ~~criativo~~ "Guga Game" foi por conta do jogador de tênis. ~~Raquete... Bolinha... Tênis...~~ :grimacing:
+
+## Como usar
--- mini_game.js
+++ mini_game.js
@@ -1,21 +1,24 @@
 
 function createAnim() {
 
+// Declarando elementos do DOM
     var $acertoPlacar = document.querySelector('#acertos');
     var $erroPlacar = document.querySelector('#erros');
     var $reset = document.querySelector('#reset');
     var $recorde = document.querySelector('#recorde');
     var $help = document.querySelector('#help');
-   
+ // Contadores dos acertos e erros
     var acerto = 0;
     var erro = 0;
 
-
+// Controle do aumento da raquete
     var aumentaPad = 0;
-        $recorde.textContent = getRecorde() || 0 ;
+
+// Setando o valor mostrado no Recorde, caso não haja um recorde na memória, ele será 0.
+    $recorde.textContent = getRecorde() || 0 ;
 
 
-  // Sounds
+  // Declarando os sons do jogo
   var soundParede = new Howl({
     src: ['sound/beep5.wav']
   });
@@ -34,13 +37,13 @@ function createAnim() {
 	});
 
 
-  // Declarando cen�rio
+  // Declarando e configurando o cenário
   var scene = oCanvas.create({
     canvas: "#canvas",
     background: "#111"
   });
 
-  // Declarando bola
+  // Declarando, configurando e adicionando a bola
   var ball = scene.display.arc({
     x: 25,
     y: 25,
@@ -53,7 +56,7 @@ function createAnim() {
   });
   scene.addChild(ball);
 
-    // Declarando raquete
+    // Declarando, configurando e adicionando a raquete
   var pad = scene.display.rectangle({
     x: 25,
     y: 400,
@@ -63,8 +66,6 @@ function createAnim() {
     velocX: 5,
     velocY: 5
   });
-
-
     scene.addChild(pad);
 
     //  Passa o movimento do mouse para o movimento do pad
@@ -72,45 +73,24 @@ function createAnim() {
       pad.x = scene.mouse.x;
     });
 
-
-    // reseta o jogo
-    $reset.addEventListener('click', function(){
-      soundReset.play();
-	  	reload(1000);	
-    });
-
-    $help.addEventListener('click', function(){
-      
-      swal({
-        text: 'Mova o mouse para guiar a raquete e não deixar a bolinha cair.\nA cada 10 acertos, a raquete irá aumentar, e a cada erro, ela diminui.',
-        icon: 'images/mouse-guide.png',
-      });
-     
-    });
-
-    
-    
-
+// Loop onde onde as ações do canvas irá acontecer
   scene.setLoop(function() {
+// Fazendo a bolinha se mover
     ball.move(ball.velocX,ball.velocY)
 
+// Fazendo a bolinha bater na parede e continuar se movendo
+	hitWall();
 
-    if ((ball.x <= 0) || (ball.x >= (scene.width))) {
-      ball.velocX = -ball.velocX;
-      soundParede.play();
-    }
-    if (ball.y < 20) {
-      ball.velocY = -ball.velocY;
-      soundParede.play();
-    }
-    if (ball.y > (pad.y - pad.height)) { //385
+// Se a bolinha acertar na raquete, continua se movendo e inclui um ponto de acerto
+    if (ball.y > (pad.y - pad.height)) {
       if ((ball.x > pad.x) && (ball.x < (pad.x + pad.width))) {
         ball.velocY = -ball.velocY;
         setAcerto();
-
-
-      } else {
-        ball.x = 25;
+      }
+/* Caso a bolinha não acerte nem a parede e nem a raquete, ela volta para seu valor de origem,
+inclui um ponto de erro e diminui a raquete */
+      else {
+        ball.x =  25; //Math.floor(Math.random() * (300 - 25 + 1)) + 25;
         ball.y = 25;
         setErro();
         }
@@ -120,7 +100,7 @@ function createAnim() {
 
   }).start();
 
-
+// Inclui ponto de acerto, ativa o som de acerto e caso tenha 10 acertos, aumenta a raquete e ativa o som de 10 pontos
   function setAcerto(){
     soundRaquete.play();
     acerto++;
@@ -133,21 +113,24 @@ function createAnim() {
     $acertoPlacar.textContent = acerto;
   }
 
+// Incluir ponto de erro, diminui a raquete e dispara os sons de erro. Caso a raquete acabe, ativa fim de jogo
   function setErro(){
     soundLose.play();
     erro++;
     $erroPlacar.textContent = erro;
-    pad.width -= 5;
+    pad.width -=5;
         if(!pad.width){
+			console.log('morreu');
           gameOver();
         }
   }
 
-
+// Pega o recorde existente na memória
     function getRecorde(){
         return localStorage.getItem('recorde');
     }
 
+// Caso acertos seja maior que recorde na memória, inclui novo recorde
    function setRecorde(){
 
         if(($acertoPlacar.textContent > getRecorde()) && ($acertoPlacar.textContent)){
@@ -156,13 +139,15 @@ function createAnim() {
         }
 
     }
-	
+
+// Recarrega a página no tempo determinado por parâmetro
 	function reload(time){
 		setTimeout(function(){
             location.reload();
         }, time);
   }
-  
+
+// Apresenta a modal de Game Over, ativa o recorde e recarrega a página
   function gameOver(){
     ball.velocX = 0;
     ball.velocY = 0;
@@ -172,9 +157,36 @@ function createAnim() {
         buttons: false,
         timer: 2000,
     });
-  reload(2500);	
+  reload(2500);
   }
-	
+
+  // Ativa botão que reseta o jogo
+      $reset.addEventListener('click', function(){
+        soundReset.play();
+  	  	reload(1000);
+      });
+
+// Ativa o bot]ao de ajuda apresentando a modal.
+      $help.addEventListener('click', function(){
+
+        swal({
+          text: 'Mova o mouse para guiar a raquete e não deixar a bolinha cair.\nA cada 10 acertos, a raquete irá aumentar, e a cada erro, ela diminui.',
+          icon: 'images/mouse-guide.png',
+        });
+
+    });
+
+// Caso a bolinha bata na parede, o som é ativado e a bolinha continua se movimentando na direção oposta
+	function hitWall(){
+		if ((ball.x <= 0) || (ball.x >= (scene.width))) {
+		      ball.velocX = -ball.velocX;
+		      soundParede.play();
+		    }
+		    if (ball.y < 20) {
+		      ball.velocY = -ball.velocY;
+		      soundParede.play();
+    }
+	}
 
 
 }
